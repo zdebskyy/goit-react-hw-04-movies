fe8d829d58babe18c6741f0cda5c726691a33322
Oleksandr Zdebskyi <technomaster@ukr.net>
2020-10-15 08:11:50 +0300

    added lazy

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import MoviesPage from "./components/MoviesPage";
-import MovieDetailsPage from "./components/MovieDetailsPage";
+import { lazy, Suspense } from "react";
 import Navigation from "./components/Navigation";
+import Loader from "react-loader-spinner";
 
 function App() {
   return (
     <div>
       <Navigation />
-
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/movies" exact component={MoviesPage} />
-        <Route path="/movies/:movieId" component={MovieDetailsPage} />
-      </Switch>
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={lazy(() =>
+              import(
+                "./components/HomePage" /* webpackChunkName: "home-page" */
+              )
+            )}
+          />
+          <Route
+            path="/movies"
+            exact
+            component={lazy(() =>
+              import(
+                "./components/MoviesPage" /* webpackChunkName: "movie-page" */
+              )
+            )}
+          />
+          <Route
+            path="/movies/:movieId"
+            component={lazy(() =>
+              import(
+                "./components/MovieDetailsPage" /* webpackChunkName: "movie-details-page" */
+              )
+            )}
+          />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
